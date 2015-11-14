00dce3cdb1302f9a09f7924843a9fd2a9f2db8a4
bvrdjagadeesh <bvrdjagadeesh@gmail.com>
2015-11-14 17:19:12 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -21,15 +21,49 @@ struct student {
 	int score;
 };
 
+void swap(struct student *students, int ind1, int ind2) {
+	struct student temp = students[ind1];
+	students[ind1] = students[ind2];
+	students[ind2] = temp;
+}
+
+void sort(struct student *students, int len){
+	int counter1, counter2;
+	for (counter1 = 0; counter1 < len - 1; counter1++){
+		for (counter2 = counter1; counter2 < len; counter2++){
+			if (students[counter1].score < students[counter2].score){
+				swap(students, counter1, counter2);
+			}
+		}
+	}
+}
+
 struct student ** topKStudents(struct student *students, int len, int K) {
 	if (K > 0 )
 	{
-		
-	}
-	else if (K > len)
-	{
-		struct student ** std = &students;
-		return std;
+		if (K == 1)
+		{
+			int counter = 0, max = 0, pos;
+			for (counter = 0; counter < len; counter++){
+				if (students[counter].score>max){
+					max = students[counter].score;
+					pos = counter;
+				}
+			}
+			struct student **topKstudents = (struct student**)calloc(K, sizeof(struct student));
+			topKstudents[0] = &students[pos];
+			return topKstudents;
+		}
+		else
+		{
+			int i;
+			struct student **topKstudents = (struct student**)calloc(K, sizeof(struct student));
+			sort(students, len);
+			for (i = 0; i < K; i++)
+				topKstudents[i] = &students[i];
+
+			return topKstudents;
+		}
 	}
 	else
 		return NULL;
